2a25545d89cb5175c62b22a1c97ba285faaee434
aista12 <sabrinaxurramova2@gmail.com>
2026-03-22 17:53:54 +0500

    setting up Docker

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="DWV Coursework", layout="wide")
+
+st.title("DWV Coursework App")
+st.write("Welcome, This app will handle data wrangling and visualization.")
